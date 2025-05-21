37c7a0a69f37bd52f1e822ed536554a9901a7f99
jazzrihal <jazzrihal@gmail.com>
2025-05-21 10:29:47 +0100

    feat: Add ESLint and Prettier tools to Mason config

--- init.lua
+++ init.lua
@@ -656,6 +656,8 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'eslint_d', -- Linter
+        'prettierd', -- Formatter
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
