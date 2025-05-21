f089774879f3fcdbeb86a6b352f93239403b597d
jazzrihal <jazzrihal@gmail.com>
2025-05-21 10:11:54 +0100

    feat: Add linter for javascript and typescript files

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -4,9 +4,15 @@ return {
     'mfussenegger/nvim-lint',
     event = { 'BufReadPre', 'BufNewFile' },
     config = function()
+      vim.env.ESLINT_D_PPID = vim.fn.getpid()
+
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
+        javascriptreact = { 'eslint_d' },
+        typescriptreact = { 'eslint_d' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
