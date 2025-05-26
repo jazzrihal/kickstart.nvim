f7bf209600014e3bcf3f451c15e3784407aa6154
jazzrihal <jazzrihal@gmail.com>
2025-05-26 23:19:19 +0100

    feat: Add C++ language support

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -13,6 +13,7 @@ return {
         typescript = { 'eslint_d' },
         javascriptreact = { 'eslint_d' },
         typescriptreact = { 'eslint_d' },
+        cpp = { 'clangtidy' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
